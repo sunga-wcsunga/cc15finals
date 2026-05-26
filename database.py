bbb3acff369ad1349df27ff9f180dd29eab209b9
sunga-wcsunga <20220026735@my.xu.edu.ph>
2026-05-26 11:17:56 +0800

    changed ip

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="192.168.254.117",
+        host="192.168.217.144",
         user="clinic_user",
         password="clinic123",
         database="ngiponch_clinic",
